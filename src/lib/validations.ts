f4e840ff2bd406ca4817bc611e0e00528305f453
Vijay S Kumar <vijay.kumar@tifin.com>
2024-10-11 18:51:47 +0530

    Feature: Auth - Adding up Validation Schema for Sign up and Login using Zod.

---
+++ src/lib/validations.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+const requiredString = z.string().trim().min(1, "Required");
+
+export const signupSchema = z.object({
+  email: requiredString.email("Invalid email address"),
+  username: requiredString.regex(
+    /^[a-zA-Z0-9_-]+$/,
+    "Invalid Username! No special characters allowed, except for _ and -"
+  ),
+  password: requiredString.min(8, "Password must be at least 8 characters"),
+});
+
+export const loginSchema = z.object({
+  username: requiredString,
+  password: requiredString,
+});
+
+export type SignUpConfig = z.infer<typeof signupSchema>;
+export type LoginConfig = z.infer<typeof loginSchema>;
