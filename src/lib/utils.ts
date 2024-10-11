471f0fb21d006c0b9589d60338af3024ece79dcc
Vijay S Kumar <vijay.kumar@tifin.com>
2024-10-11 19:49:41 +0530

    Tweak: Adding an utility method to create a session from user id.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import { luciaAuth } from "@/auth";
+import { cookies } from "next/headers";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const createSessionFromUserId = async (userId: string) => {
+  const session = await luciaAuth.createSession(userId, {});
+  const sessionCookie = luciaAuth.createSessionCookie(session.id);
+
+  // Set the session cookie for the user
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+};
