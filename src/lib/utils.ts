f5878fcbe322ac18b330e1793868c0e4a377b616
Vijay S Kumar <vijay.kumar@tifin.com>
2024-10-11 22:49:27 +0530

    Tweak: Adding a ref to understand the cn function of tailwind-merge better.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// https://medium.com/@naglaafouz4/enhancing-component-reusability-in-tailwind-css-with-clsx-and-tailwind-merge-986aa4e1fe76
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
