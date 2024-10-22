209f53adee845226f9c3e854821b9b58539876b4
vijays-hub <vijayskumar82@gmail.com>
2024-10-22 22:03:57 +0530

    Tweak - Adding re-usable UserButton and User Avatar components!

---
+++ src/components/UserButton.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useSession } from "@/app/(main)/SessionProvider";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "./ui/dropdown-menu";
+import UserAvatar from "./UserAvatar";
+import Link from "next/link";
+import { LogOutIcon, UserIcon } from "lucide-react";
+import { logout } from "@/app/(auth)/actions";
+import { cn } from "@/lib/utils";
+
+interface UserButtonConfig {
+  className?: string;
+}
+
+export default function UserButton({ className }: UserButtonConfig) {
+  const { user } = useSession();
+
+  return (
+    <DropdownMenu>
+      {/* ? Find out the significance of asChild here */}
+      <DropdownMenuTrigger asChild>
+        <button className={cn("flex-none rounded-full", className)}>
+          <UserAvatar avatarUrl={user.avatarUrl} size={40} />
+        </button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Logged in as @{user.username}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <Link href={`/users/${user.username}`}>
+          <DropdownMenuItem>
+            <UserIcon className="mr-2 size-4" />
+            Profile
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => {
+            logout();
+          }}
+        >
+          <LogOutIcon className="mr-2 size-4" />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
