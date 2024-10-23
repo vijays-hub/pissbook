570d915d9c2ce7408e33ddd8286635b16f205fa5
vijays-hub <vijayskumar82@gmail.com>
2024-10-23 13:45:18 +0530

    Feature: Posts - Adding an utility method for displaying relative time.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,35 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { formatDate, formatDistanceToNowStrict } from "date-fns";
 
 // https://medium.com/@naglaafouz4/enhancing-component-reusability-in-tailwind-css-with-clsx-and-tailwind-merge-986aa4e1fe76
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+/**
+ * We are using date-fns library to format the date.
+ *
+ * Read more:
+ * 1. https://date-fns.org/v4.1.0/docs/formatDistanceToNowStrict
+ * 2. https://date-fns.org/v4.1.0/docs/format
+ */
+export function formatRelativeDate(from: Date) {
+  const currentDate = new Date();
+
+  if (currentDate.getTime() - from.getTime() < 24 * 60 * 60 * 1000) {
+    // The post was created today, hence show the relative time like "2 hours ago"
+    return formatDistanceToNowStrict(from, { addSuffix: true });
+  } else {
+    /**
+     * The post was created on a different day, hence show the date like "Jul 12" if
+     * the post was created in the current year, or show the date like "Jul 12, 2022"
+     * if the post was created in a different year.
+     */
+    if (currentDate.getFullYear() === from.getFullYear()) {
+      return formatDate(from, "MMM d");
+    } else {
+      return formatDate(from, "MMM d, yyyy");
+    }
+  }
+}
