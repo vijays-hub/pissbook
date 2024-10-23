daa43aa4304bd2641d3d6cc2fe4209cbae55d321
vijays-hub <vijayskumar82@gmail.com>
2024-10-23 13:46:20 +0530

    Feature: Posts - Displaying existing user posts.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,25 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
+import Post from "@/components/posts/Post";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
 
-export default function Home() {
+export default async function Home() {
+  // TEMP: Displaying Posts directly. Will be refactoring this later.
+  const posts = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: {
+      createdAt: "desc", // show the latest posts first
+    },
+  });
+
   return (
-    <main className="w-full">
-      <div className="w-full">
+    <main className="w-full min-w-0">
+      <div className="w-full min-w-0 space-y-5">
         <PostEditor />
+
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
       </div>
     </main>
   );
---
+++ src/components/posts/Post.tsx
@@ -0,0 +1,59 @@
+/**
+ * import { Post as PostSchema } from "@prisma/client";
+ * interface PostConfig {
+ *      post: PostSchema;
+ * }
+ *
+ * The default type the @prisma/client library returns won't have the relations (include) data.
+ * That is, with the above code, for a post, there is a relation between the post and the user.
+ * But when you try to read the data of 'post', the user won't be included. To fix this, we
+ * are creating a new type for ourselves that includes the user data. This also helps us to
+ * re-use the type in multiple places. So, whenever we are querying for posts, we can simply
+ * specify this type in the include field of findMany query of posts.
+ *
+ * Checkout the PostData type in src/lib/types.ts file. It is a type that includes the user data
+ * along with the post data. This is the type we are using in the Post component.
+ *
+ * interface PostConfig {
+ *     post: PostData;
+ * }
+ *
+ */
+
+import { PostData } from "@/lib/types";
+import Link from "next/link";
+import UserAvatar from "../UserAvatar";
+import { formatRelativeDate } from "@/lib/utils";
+
+interface PostConfig {
+  post: PostData;
+}
+
+export default function Post({ post }: PostConfig) {
+  return (
+    <article className="space-y-3 bg-card rounded-2xl p-5 shadow-sm">
+      <div className="flex flex-wrap gap-3">
+        <Link href={`/users/${post.user.username}`}>
+          <UserAvatar avatarUrl={post.user.avatarUrl} />
+        </Link>
+
+        <div>
+          <Link
+            href={`/users/${post.user.username}`}
+            className="block font-medium hover:underline capitalize"
+          >
+            {post.user.displayName}
+          </Link>
+          <Link
+            href={`/posts/${post.id}`}
+            className="block text-sm text-muted-foreground hover:underline"
+          >
+            {formatRelativeDate(post.createdAt)}
+          </Link>
+        </div>
+      </div>
+
+      <div className="whitespace-pre-line break-words">{post.content}</div>
+    </article>
+  );
+}
