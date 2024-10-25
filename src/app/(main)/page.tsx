85572a4d1f5956e06881fcf4928454c0bb191334
vijays-hub <vijayskumar82@gmail.com>
2024-10-25 16:51:58 +0530

    Feature: For You Feed - Integrating for-you feed and caching the response using react-query

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,26 +1,14 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
-import Post from "@/components/posts/Post";
 import TrendingTopics from "@/components/TrendingTopics";
-import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import ForYouFeed from "./ForYouFeed";
 
-export default async function Home() {
-  // TEMP: Displaying Posts directly. Will be refactoring this later.
-  const posts = await prisma.post.findMany({
-    include: postDataInclude,
-    orderBy: {
-      createdAt: "desc", // show the latest posts first
-    },
-  });
-
+export default function Home() {
   return (
     <main className="w-full min-w-0 flex gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
 
-        {posts.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
+        <ForYouFeed />
       </div>
 
       <TrendingTopics />
