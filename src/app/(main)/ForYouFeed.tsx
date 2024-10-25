3d4cbf7f46f4b0a98cec07ab19a90636fcc32dff
vijays-hub <vijayskumar82@gmail.com>
2024-10-25 17:44:35 +0530

    CONFIG: Setting up a simple http client named "ky" which is based on Javascript's fetch.

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Post from "@/components/posts/Post";
+import kyInstance from "@/lib/ky";
 import { PostData } from "@/lib/types";
 import { useQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
@@ -9,16 +10,8 @@ export default function ForYouFeed() {
   const query = useQuery<PostData[]>({
     // Adding a generic "post-feed" key to the queryKey. We'll use this for all our post feed queries.
     queryKey: ["post-feed", "for-you"],
-    queryFn: async () => {
-      // We'll use a common util for network requests later. For now, we'll use fetch.
-      const response = await fetch("/api/posts/for-you");
-      if (!response.ok) {
-        throw new Error(
-          `Failed to fetch the feed. Errored with status code ${response.status}`
-        );
-      }
-      return response.json();
-    },
+    // No need to handle any error states. kyInstance will throw an error if the request fails.
+    queryFn: kyInstance.get("/api/posts/for-you").json<PostData[]>,
   });
 
   if (query.status === "pending") {
@@ -31,10 +24,10 @@ export default function ForYouFeed() {
 
   //   When data is available, we can render the posts.
   return (
-    <>
+    <div className="space-y-5">
       {query.data.map((post) => (
         <Post key={post.id} post={post} />
       ))}
-    </>
+    </div>
   );
 }
