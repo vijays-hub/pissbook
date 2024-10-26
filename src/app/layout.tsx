7f49766088946beb8ef326a9d1ca6814dc9170d9
vijays-hub <vijayskumar82@gmail.com>
2024-10-26 12:59:33 +0530

    Tweak: Adding Toaster wrapper for displaying posts in the app.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import ReactQueryClientProvider from "./ReactQueryClientProvider";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -68,6 +69,7 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
         </ReactQueryClientProvider>
+        <Toaster />
       </body>
     </html>
   );
