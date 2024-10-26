bb875f42b378dec84d05938fba77e20c9363140f
vijays-hub <vijayskumar82@gmail.com>
2024-10-26 12:15:36 +0530

    Feature: Infinite Scrolling - Loading the feed on demand (Infinite Loading) with the help of `useInfiniteQuery` hook!

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -1,33 +1,84 @@
 "use client";
 
+import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
 import Post from "@/components/posts/Post";
 import kyInstance from "@/lib/ky";
-import { PostData } from "@/lib/types";
-import { useQuery } from "@tanstack/react-query";
+import { PostsPage } from "@/lib/types";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
 
 export default function ForYouFeed() {
-  const query = useQuery<PostData[]>({
+  // This is a callback function that will be called by useInfiniteQuery to fetch the posts.
+  const fetchPosts = async ({ pageParam }: { pageParam: string | null }) =>
+    kyInstance
+      .get(
+        "/api/posts/for-you",
+        pageParam
+          ? {
+              searchParams: { cursor: pageParam },
+            }
+          : {}
+      )
+      .json<PostsPage>();
+
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
     // Adding a generic "post-feed" key to the queryKey. We'll use this for all our post feed queries.
     queryKey: ["post-feed", "for-you"],
     // No need to handle any error states. kyInstance will throw an error if the request fails.
-    queryFn: kyInstance.get("/api/posts/for-you").json<PostData[]>,
+    queryFn: fetchPosts,
+    /**
+     * The first page will have a null cursor. Also, we have strictly typed this to
+     * be a string or null. This is because the cursor can be string or null. If it is
+     * null, it means there are no more posts available.
+     */
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
   });
 
-  if (query.status === "pending") {
+  /**
+   * 'data' object looks like this:
+   * { pages: [{ posts: PostData[], nextCursor: string | null }], pageParams: string[] }
+   *
+   * So we need to flatten the 'pages' array to get all the posts.
+   *
+   * What is flatten? -- It is a process of converting a 2D array to a 1D array.
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+   *
+   * flatMap is a combination of map and flat. It maps each element using the map function.
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flatMap
+   *
+   * We are using flatMap to flatten the 'pages' array and then map over each page to get the posts.
+   * The posts array present in each object of pages is what we are trying to flatten.̊
+   *
+   */
+  const posts = data?.pages.flatMap((page) => page.posts) ?? [];
+
+  if (status === "pending") {
     return <Loader2 className="mx-auto animate-spin" />;
   }
 
-  if (query.status === "error") {
+  if (status === "error") {
     return <p>Failed to fetch the feed for you. Please try again later.</p>;
   }
 
   //   When data is available, we can render the posts.
   return (
-    <div className="space-y-5">
-      {query.data.map((post) => (
+    <InfiniteScrollContainer
+      className="space-y-5"
+      onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+    >
+      {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
-    </div>
+
+      {isFetchingNextPage && <Loader2 className="mx-auto my-3 animate-spin" />}
+    </InfiniteScrollContainer>
   );
 }
