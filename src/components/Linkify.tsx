9a93503d073d7447d7b30ee17693b7f9bddcaff8
vijays-hub <vijayskumar82@gmail.com>
2024-10-27 19:54:13 +0530

    Tweak: Wrapping the linkified username with Tooltip.

--- src/components/Linkify.tsx
+++ src/components/Linkify.tsx
@@ -4,6 +4,7 @@
  */
 import Link from "next/link";
 import { LinkIt, LinkItUrl } from "react-linkify-it";
+import UserLinkWithTooltip from "./UserLinkWithTooltip";
 
 interface LinkifyConfig {
   children: React.ReactNode;
@@ -25,6 +26,28 @@ function LinkifyUrl({ children }: LinkifyConfig) {
   );
 }
 
+/**
+ * This component will convert any username into a link, that can be navigated to
+ * the user's profile.
+ * 
+ * Before Tooltip we used to return the following snippet inside component param:
+ * 
+ *    <Link
+        href={`/users/${username}`}
+        className="text-primary hover:underline"
+      >
+        {match}
+      </Link>
+ * 
+ *
+ * Now, this is also a good place to add our newly introduced UserTooltip component.
+ * But wait, the children for this component is a mere string and we don't have access
+ * to the user's data. Hence, we are using a separate endpoint to fetch the user's data.
+ * Using react-query to cache the user's data, so that we need not fetch the same user's
+ * data again and again.
+ *
+ * Checkout -> src/components/UserLinkWithTooltip.tsx
+ */
 function LinkifyUsername({ children }: LinkifyConfig) {
   return (
     <LinkIt
@@ -33,12 +56,9 @@ function LinkifyUsername({ children }: LinkifyConfig) {
         const username = match.slice(1); // remove the @ from the username
 
         return (
-          <Link
-            href={`/users/${username}`}
-            className="text-primary hover:underline"
-          >
+          <UserLinkWithTooltip username={username} key={key}>
             {match}
-          </Link>
+          </UserLinkWithTooltip>
         );
       }}
     >
