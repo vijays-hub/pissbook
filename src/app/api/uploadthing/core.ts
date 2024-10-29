798e9af9c5c500ae8d1cd483ed4f0e386566dd61
vijays-hub <vijayskumar82@gmail.com>
2024-10-29 19:30:06 +0530

    Feature: Edit Profile - Deleting old files with the same key from upload thing dashboard when updating the avatar of user.

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -7,7 +7,7 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
 import { createUploadthing, FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+import { UploadThingError, UTApi } from "uploadthing/server";
 
 const uploadInstance = createUploadthing();
 
@@ -24,6 +24,23 @@ export const fileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       /**
+       * Since we can have only one Avatar for a user, there is no point keeping them
+       * in the upload thing files database (you can check that in the dashboard). So
+       * when user is trying to upload a new avatar, we will delete the old one using the
+       * key of the old avatar.
+       */
+      const oldAvatarUrl = metadata.user.avatarUrl;
+
+      if (oldAvatarUrl) {
+        // Check below comment to understand this.
+        const key = oldAvatarUrl.split(
+          `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`
+        )[1];
+
+        await new UTApi().deleteFiles(key);
+      }
+
+      /**
        * The default file URL that uploadthing generates is not a secure one.
        * To make it secure, they advice us to use the APP_ID in the URL.
        * Hence, we construct the URL ourselves.
@@ -32,10 +49,10 @@ export const fileRouter = {
        *
        * We replace the `f` with the APP_ID to make it secure. Uploadthing suggests this
        * URL for security -> https://utfs.io/a/<APP_ID>/<FILE_KEY>
-       * 
+       *
        * Also, to use such URLs in NextJs, make sure you whitelist the domain in the `next.config.js` file.
        * This is needed when you are using the `Image` component from NextJs.
-       * 
+       *
        *
        * Learn more: Learn more on working with files -> https://docs.uploadthing.com/working-with-files
        */
