277b07378a587c878cc14017eb1d107a7cef47e5
vijayskumar82@gmail.com <vijayskumar82@gmail.com>
2024-11-03 12:39:25 +0530

    Feature: Comments - Adding a mutation to submit a comment and invalidate the cache as and when a new comment is added!

---
+++ src/components/posts/comments/mutations.ts
@@ -0,0 +1,69 @@
+import { CommentsPage } from "@/lib/types";
+import {
+  InfiniteData,
+  QueryKey,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { deleteComment, submitComment } from "./actions";
+import { toast } from "@/hooks/use-toast";
+
+/**
+ * Have a look at the for-you feed mutation to digest what's happening here.
+ * (src/components/posts/editor/mutations.ts)
+ *
+ * Although we are doing reverse pagination here, you could relate most of
+ * this code to the for-you feed mutation.
+ */
+export function useSubmitCommentMutation(postId: string) {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: submitComment,
+    onSuccess: async (newComment) => {
+      const queryKey: QueryKey = ["comments", postId];
+
+      await queryClient.cancelQueries({ queryKey });
+
+      queryClient.setQueryData<InfiniteData<CommentsPage, string | null>>(
+        queryKey,
+        (oldData) => {
+          const firstPage = oldData?.pages[0];
+
+          if (firstPage) {
+            return {
+              pageParams: oldData.pageParams,
+              pages: [
+                {
+                  previousCursor: firstPage.previousCursor,
+                  comments: [...firstPage.comments, newComment],
+                },
+                ...oldData.pages.slice(1),
+              ],
+            };
+          }
+        }
+      );
+
+      queryClient.invalidateQueries({
+        queryKey,
+        predicate(query) {
+          return !query.state.data;
+        },
+      });
+
+      toast({
+        description: "Added your opinion to this pissing experience",
+      });
+    },
+    onError(error) {
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Failed to add your opinion. Please try again.",
+      });
+    },
+  });
+
+  return mutation;
+}
