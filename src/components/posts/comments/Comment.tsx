379d537c351edd9b12962b515e90d8457c24433d
vijayskumar82@gmail.com <vijayskumar82@gmail.com>
2024-11-03 13:16:17 +0530

    Feature: Comments - Fetching and Rendering the comments on a post!

---
+++ src/components/posts/comments/Comment.tsx
@@ -0,0 +1,37 @@
+import { useSession } from "@/app/(main)/SessionProvider";
+import UserAvatar from "@/components/UserAvatar";
+import UserTooltip from "@/components/UserTooltip";
+import { CommentData } from "@/lib/types";
+import { formatRelativeDate } from "@/lib/utils";
+import Link from "next/link";
+
+export default function Comment({ comment }: { comment: CommentData }) {
+  return (
+    <div className="group/comment flex gap-3 py-3">
+      <span className="hidden sm:inline">
+        <UserTooltip user={comment.user}>
+          <Link href={`/users/${comment.user.username}`}>
+            <UserAvatar avatarUrl={comment.user.avatarUrl} size={40} />
+          </Link>
+        </UserTooltip>
+      </span>
+      <div>
+        <div className="flex items-center gap-1 text-sm">
+          <UserTooltip user={comment.user}>
+            <Link
+              href={`/users/${comment.user.username}`}
+              className="font-medium hover:underline"
+            >
+              {comment.user.displayName}
+            </Link>
+          </UserTooltip>
+          •
+          <span className="text-muted-foreground">
+            {formatRelativeDate(comment.createdAt)}
+          </span>
+        </div>
+        <div>{comment.content}</div>
+      </div>
+    </div>
+  );
+}
